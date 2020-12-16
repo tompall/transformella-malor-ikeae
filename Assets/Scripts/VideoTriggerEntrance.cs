6202fe10cfa909ac338e89a7f1104ae166b5e9df
Jelena Viskovic <jelena.viskovic@gmail.com>
2020-12-16 13:17:37 +0000

    test-01

---
+++ Assets/Scripts/VideoTriggerEntrance.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoTriggerEntrance : MonoBehaviour
+{
+
+    public GameObject[] videoArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject vid in videoArray)
+        {
+            vid.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject vid in videoArray)
+        {
+            vid.SetActive(true);
+        }
+    }
+}
