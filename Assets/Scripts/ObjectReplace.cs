eb94ef4d9fff67a1afc5252a41af136b29e83aed
Jelena Viskovic <jelena.viskovic@gmail.com>
2020-12-17 18:25:22 +0000

    replaceobjecttouch

---
+++ Assets/Scripts/ObjectReplace.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectReplace : MonoBehaviour
+{
+    public GameObject toReplace;
+    public GameObject replaceWith;
+
+
+    public void Start()
+    {
+        toReplace.SetActive(true);
+        replaceWith.SetActive(false);
+    }
+    public void ReplaceObjects()
+    {
+        toReplace.SetActive(false);
+        replaceWith.SetActive(true);
+    }
+}
