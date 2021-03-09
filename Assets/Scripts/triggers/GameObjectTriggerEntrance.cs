6fddbfe8b504f847371d82bbbc1ae76d2041bc24
Jelena Viskovic <jelena.viskovic@gmail.com>
2021-03-09 11:18:42 +0000

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/triggers/GameObjectTriggerEntrance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectTriggerEntrance : MonoBehaviour
+{
+
+    public GameObject[] objectsToActivate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject vid in objectsToActivate)
+        {
+            vid.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject vid in objectsToActivate)
+        {
+            vid.SetActive(true);
+        }
+    }
+}
