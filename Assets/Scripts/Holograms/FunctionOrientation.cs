26b00404f2420a71466fb3a1c9949e8cc5a59b37
Jelena Viskovic <jelena.viskovic@gmail.com>
2021-03-10 22:27:51 +0000

    updates

---
+++ Assets/Scripts/Holograms/FunctionOrientation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionOrientation : MonoBehaviour
+{
+    public GameObject menu; // Get a reference to the menu gameobject
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu = this.gameObject;
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 devicePosition = Camera.main.transform.position;
+
+        menu.transform.position = new Vector3(devicePosition.x, devicePosition.y, devicePosition.z + 2.5f);
+
+        Vector3 gazeDirection = Camera.main.transform.forward;
+
+        menu.transform.rotation =  Quaternion.LookRotation(gazeDirection)* Quaternion.Euler(0, 90, 0);
+    }
+}
---
+++ Assets/Scripts/shrine/EnableVideosLines.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableVideosLines : MonoBehaviour
+{
+
+    public List <GameObject> videosToEnable;
+
+    public GameObject horizontalLines;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int count = transform.childCount;
+        for (int i = 0; i < count; i++)
+        {
+            Transform child = transform.GetChild(i);
+            videosToEnable.Add(child.gameObject);
+        }
+
+        StartCoroutine(TurnVideosOn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator TurnVideosOn()
+    {
+        foreach (GameObject go in videosToEnable)
+        {
+            go.SetActive(true);
+            yield return new WaitForSeconds (0.5f);
+        }
+        horizontalLines.SetActive(true);
+
+        StartCoroutine(TurnVideosOff());
+        yield return null;
+    }
+
+    public IEnumerator TurnVideosOff()
+    {
+
+        yield return new WaitForSeconds(220f);
+        foreach (GameObject go in videosToEnable)
+        {
+            go.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+        }
+        horizontalLines.SetActive(false);
+        yield return null;
+    }
+}
