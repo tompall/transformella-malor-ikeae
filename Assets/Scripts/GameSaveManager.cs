cea4d0c237f33955cfcb6e95205a69bb87b2e421
Jelena Viskovic <jelena.viskovic@gmail.com>
2021-03-18 01:07:23 +0000

    update

--- Assets/Scripts/GameSaveManager.cs
+++ Assets/Scripts/GameSaveManager.cs
@@ -9,7 +9,7 @@ using Newtonsoft.Json;
 public class GameSaveManager : MonoBehaviour
 {
 
-   
+    private string jsonPath = "/game_save/hologram_save.json";
     public List<HologramItemData> dataObjects;
 
     public SaveItems si = new SaveItems();
@@ -37,33 +37,42 @@ public class GameSaveManager : MonoBehaviour
         return Directory.Exists(Application.persistentDataPath + "/game_save");
     }
 
+    public bool DoesFileExists()
+    {
+        return IsSaveFile() && File.Exists(Application.persistentDataPath + jsonPath);
+    }
+
     public void LoadGame()
     {
-        string path = Application.persistentDataPath + "/game_save/hologram_data/hologram_save.json";
-        byte[] data = UnityEngine.Windows.File.ReadAllBytes(path);
-        string json = Encoding.ASCII.GetString(data);
-
-        var convertedJson = JsonConvert.DeserializeObject<SaveItems>(json);
-        si.SetHologramitems(convertedJson.GetHologramitems());
-        Debug.Log("LOADING: " + si.hologramItems[0].localPosY);
-
-        for (int i = 0; i < si.GetHologramitems().Count; i++)
+        if (DoesFileExists())
         {
-            dataObjects[i].localPosX = si.GetHologramitems()[i].localPosX;
-            dataObjects[i].localPosY = si.GetHologramitems()[i].localPosY;
-            dataObjects[i].localPosZ = si.GetHologramitems()[i].localPosZ;
+            string path = Application.persistentDataPath + jsonPath;
+            byte[] data = UnityEngine.Windows.File.ReadAllBytes(path);
+            string json = Encoding.ASCII.GetString(data);
 
-            dataObjects[i].localRotx = si.GetHologramitems()[i].localRotx;
-            dataObjects[i].localRoty = si.GetHologramitems()[i].localRoty;
-            dataObjects[i].localRotz = si.GetHologramitems()[i].localRotz;
+            var convertedJson = JsonConvert.DeserializeObject<SaveItems>(json);
+            si.SetHologramitems(convertedJson.GetHologramitems());
+            Debug.Log("LOADING: " + si.hologramItems[0].localPosY);
 
-            dataObjects[i].localSx = si.GetHologramitems()[i].localSx;
-            dataObjects[i].localSy = si.GetHologramitems()[i].localSy;
-            dataObjects[i].localSz = si.GetHologramitems()[i].localSz;
+            for (int i = 0; i < si.GetHologramitems().Count; i++)
+            {
+                dataObjects[i].localPosX = si.GetHologramitems()[i].localPosX;
+                dataObjects[i].localPosY = si.GetHologramitems()[i].localPosY;
+                dataObjects[i].localPosZ = si.GetHologramitems()[i].localPosZ;
+
+                dataObjects[i].localRotx = si.GetHologramitems()[i].localRotx;
+                dataObjects[i].localRoty = si.GetHologramitems()[i].localRoty;
+                dataObjects[i].localRotz = si.GetHologramitems()[i].localRotz;
+
+                dataObjects[i].localSx = si.GetHologramitems()[i].localSx;
+                dataObjects[i].localSy = si.GetHologramitems()[i].localSy;
+                dataObjects[i].localSz = si.GetHologramitems()[i].localSz;
+            }
+
+            //dataObjects = si.GetHologramitems();
+            Debug.Log("LOADING dataObjects: " + dataObjects[0].localPosY);
         }
-
-        //dataObjects = si.GetHologramitems();
-        Debug.Log("LOADING dataObjects: " + dataObjects[0].localPosY);
+ 
     }
 
     public void SaveGame()
@@ -72,12 +81,8 @@ public class GameSaveManager : MonoBehaviour
         {
             Directory.CreateDirectory(Application.persistentDataPath + "/game_save");
         }
-        if (!Directory.Exists(Application.persistentDataPath + "/game_save/hologram_data"))
-        {
-            Directory.CreateDirectory(Application.persistentDataPath + "/game_save/hologram_data");
-        }
 
-        string path = Application.persistentDataPath + "/game_save/hologram_data/hologram_save.json";
+        string path = Application.persistentDataPath + "/game_save/hologram_save.json";
         si.SetHologramitems(dataObjects);
         Debug.Log("SAVING dataObjects: " + dataObjects[0].localPosY);
         string json = JsonConvert.SerializeObject(si);
