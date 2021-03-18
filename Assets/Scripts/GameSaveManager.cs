41c45771f04e06b667b8566dce28dff67f212a8d
Jelena Viskovic <jelena.viskovic@gmail.com>
2021-03-18 00:54:46 +0000

    saving

--- Assets/Scripts/GameSaveManager.cs
+++ Assets/Scripts/GameSaveManager.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using Newtonsoft.Json;
 
 public class GameSaveManager : MonoBehaviour
 {
 
    
-    public HologramItemData dataObjects;
+    public List<HologramItemData> dataObjects;
+
+    public SaveItems si = new SaveItems();
 
     public void Awake()
     {
-        
+       si.SetHologramitems(dataObjects);
+
+        LoadGame();
 
     }
 
@@ -23,10 +29,7 @@ public class GameSaveManager : MonoBehaviour
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            LoadGame();
-        }
+        
     }
 
     public bool IsSaveFile()
@@ -34,7 +37,34 @@ public class GameSaveManager : MonoBehaviour
         return Directory.Exists(Application.persistentDataPath + "/game_save");
     }
 
+    public void LoadGame()
+    {
+        string path = Application.persistentDataPath + "/game_save/hologram_data/hologram_save.json";
+        byte[] data = UnityEngine.Windows.File.ReadAllBytes(path);
+        string json = Encoding.ASCII.GetString(data);
 
+        var convertedJson = JsonConvert.DeserializeObject<SaveItems>(json);
+        si.SetHologramitems(convertedJson.GetHologramitems());
+        Debug.Log("LOADING: " + si.hologramItems[0].localPosY);
+
+        for (int i = 0; i < si.GetHologramitems().Count; i++)
+        {
+            dataObjects[i].localPosX = si.GetHologramitems()[i].localPosX;
+            dataObjects[i].localPosY = si.GetHologramitems()[i].localPosY;
+            dataObjects[i].localPosZ = si.GetHologramitems()[i].localPosZ;
+
+            dataObjects[i].localRotx = si.GetHologramitems()[i].localRotx;
+            dataObjects[i].localRoty = si.GetHologramitems()[i].localRoty;
+            dataObjects[i].localRotz = si.GetHologramitems()[i].localRotz;
+
+            dataObjects[i].localSx = si.GetHologramitems()[i].localSx;
+            dataObjects[i].localSy = si.GetHologramitems()[i].localSy;
+            dataObjects[i].localSz = si.GetHologramitems()[i].localSz;
+        }
+
+        //dataObjects = si.GetHologramitems();
+        Debug.Log("LOADING dataObjects: " + dataObjects[0].localPosY);
+    }
 
     public void SaveGame()
     {
@@ -46,31 +76,13 @@ public class GameSaveManager : MonoBehaviour
         {
             Directory.CreateDirectory(Application.persistentDataPath + "/game_save/hologram_data");
         }
-        BinaryFormatter bf = new BinaryFormatter();
 
-        FileStream file = File.Create(Application.persistentDataPath + "/game_save/hologram_data/hologram_save.txt");
+        string path = Application.persistentDataPath + "/game_save/hologram_data/hologram_save.json";
+        si.SetHologramitems(dataObjects);
+        Debug.Log("SAVING dataObjects: " + dataObjects[0].localPosY);
+        string json = JsonConvert.SerializeObject(si);
+        byte[] data = Encoding.ASCII.GetBytes(json);
 
-        var json = JsonUtility.ToJson(dataObjects);
-        bf.Serialize(file, json);
-        file.Close();
-    }
-
-    public void LoadGame()
-    {
-
-        Debug.Log("loadingnnnn");
-        if (!Directory.Exists(Application.persistentDataPath + "/game_save/hologram_data"))
-        {
-            Directory.CreateDirectory(Application.persistentDataPath + "/game_save/hologram_data");
-        }
-
-        BinaryFormatter bf = new BinaryFormatter();
-
-        if (File.Exists(Application.persistentDataPath + "/game_save/hologram_data/hologram_save.txt"))
-        {
-           FileStream file =  File.Open(Application.persistentDataPath + "/game_save/hologram_data/hologram_save.txt", FileMode.Open);
-            JsonUtility.FromJsonOverwrite((string)bf.Deserialize(file), dataObjects);
-            file.Close();
-        }
+        UnityEngine.Windows.File.WriteAllBytes(path, data);
     }
 }
---
+++ Assets/Scripts/SaveItems.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveItems
+{
+    public List<HologramItemData> hologramItems;
+
+    public void SetHologramitems(List<HologramItemData> _hologramItems)
+    {
+        hologramItems = _hologramItems;
+    }
+    
+    public List<HologramItemData> GetHologramitems()
+    {
+        return hologramItems;
+    }
+}
