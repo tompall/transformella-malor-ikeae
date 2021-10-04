922b6af090463523418e83865595f3f4afba77b5
tompall <pall.tamas1@gmail.com>
2021-10-04 21:45:13 +0200

    test 3

--- Assets/Scripts/Narrative/new-system/NSys.cs
+++ Assets/Scripts/Narrative/new-system/NSys.cs
@@ -47,7 +47,7 @@ public class NSys : MonoBehaviour
     {
         visualGuide.ShowVisual(false);
 
-        //line.material = new Material(Shader.Find("Sprites/Default"));
+        line.material = new Material(Shader.Find("Sprites/Default"));
         //DrawGuideLine(); 
 
     }
@@ -79,22 +79,22 @@ public class NSys : MonoBehaviour
 
     public void DrawGuideLine()
     {
-        //canUpdateLine = true;
-        //line.positionCount = triggers.Count;
+        canUpdateLine = true;
+        line.positionCount = triggers.Count;
 
-        //yOffset = -1f;
+        yOffset = -1f;
 
-        //linePos = Vector3.one;
+        linePos = Vector3.one;
 
-        //lineIndex = 0;
-        //foreach (var trigger in triggers)
-        //{
-        //    linePos = trigger.orbLocation.position;
+        lineIndex = 0;
+        foreach (var trigger in triggers)
+        {
+            linePos = trigger.orbLocation.position;
 
-        //    linePos.y = yOffset;
-        //    line.SetPosition(lineIndex, linePos);
-        //    lineIndex++;
-        //}
+            linePos.y = yOffset;
+            line.SetPosition(lineIndex, linePos);
+            lineIndex++;
+        }
     }
 
     public void SetLineFromStartToWomb()
