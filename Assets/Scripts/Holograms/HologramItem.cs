e59aecd1365019f514b32447b15b6ee152a28f96
tompall <pall.tamas1@gmail.com>
2021-10-05 20:34:18 +0200

    trigger scene updated with new assets

--- Assets/Scripts/Holograms/HologramItem.cs
+++ Assets/Scripts/Holograms/HologramItem.cs
@@ -46,6 +46,20 @@ public class HologramItem : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        this.transform.localPosition = new Vector3(ItemData.localPosX, ItemData.localPosY, ItemData.localPosZ);
+        this.transform.localRotation = Quaternion.Euler(ItemData.localParentRotx, ItemData.localParentRoty, ItemData.localParentRotz);
+
+        // Quaternion m_HologramTargetRot *= Quaternion.Euler(ItemData.localRotx, ItemData.localRoty, ItemData.localRotz);
+        hologramToSave.transform.localRotation = Quaternion.Euler(ItemData.localRotx, ItemData.localRoty, ItemData.localRotz);
+        //   mesh.transform.localScale = new Vector3(pinchSlider.SliderValue, pinchSlider.SliderValue, pinchSlider.SliderValue);
+        if (saveScale)
+        {
+            this.transform.localScale = new Vector3(ItemData.localSx, ItemData.localSy, ItemData.localSz);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
