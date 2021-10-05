dc7f204e3138259a0875eeabd1a7e1ca31a7678c
tompall <pall.tamas1@gmail.com>
2021-10-05 13:29:32 +0200

    TIMELINE PROTOTYPE

---
+++ Assets/Scripts/TIMELINE/ActionOnActivation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionOnActivation : MonoBehaviour
+{
+
+    public UnityEvent OnActivate = new UnityEvent();
+    private void OnEnable()
+    {
+        OnActivate.Invoke();
+    }
+}
