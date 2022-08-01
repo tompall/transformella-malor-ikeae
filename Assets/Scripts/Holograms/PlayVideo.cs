14c2bf1c2e2bcc88e2106f4009c6b54bff3d18fa
tompall <pall.tamas1@gmail.com>
2022-08-01 15:45:47 +0200

    shedhalle rearrange & new assets

---
+++ Assets/Scripts/Holograms/PlayVideo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour
+{
+    public VideoPlayer player;
+    private void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+    }
+    public void PlayVideoTrigger()
+    {
+        player.Play();
+    }
+}
