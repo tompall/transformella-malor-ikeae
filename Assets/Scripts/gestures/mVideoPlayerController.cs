e39a4188a0b16414e22770bc4c73a9a8255f0c89
tompall <pall.tamas1@gmail.com>
2022-08-02 16:46:09 +0200

    negentropy video interactive integration & new video for Lonite

---
+++ Assets/Scripts/gestures/mVideoPlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class mVideoPlayerController : MonoBehaviour
+{
+    public VideoPlayer player;
+
+    public void ToggleVideo()
+    {
+        if (player.isPlaying)
+        {
+            player.Pause();
+        }
+        else
+        {
+            player.Play();
+        }
+    }
+}
