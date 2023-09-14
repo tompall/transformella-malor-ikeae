2c76542f314cd95e5d0a669c387df27a64100d96
Tamas Pall <pall.tamas1@gmail.com>
2023-09-14 11:27:27 +0200

    updated ceramics scene

---
+++ Assets/Scripts/shrine/TurnOffVideo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TurnOffVideo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<VideoPlayer>().loopPointReached += EndReached;
+
+    }
+
+    private void EndReached(VideoPlayer source)
+    {
+        gameObject.SetActive(false);
+    }
+}
